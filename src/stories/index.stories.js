982ae64c7db5079431d4a72e3f178773296b41ea
Rasmus Schlünsen <rasmus@schlunsen.com>
2021-05-05 13:38:33 +0200

    Added new test. Updated npm packages

--- src/stories/index.stories.js
+++ src/stories/index.stories.js
@@ -7,12 +7,32 @@ export default {
   title: "Gauge test",
 };
 
-
-
 export const simpleGaugeTest = () => ({
   components: { Gauge },
-  template: '<gauge />',
+  template: "<gauge />",
   methods: { action: action("clicked") },
 });
 
-
+export const customizedGaugeTest = () => ({
+  components: { Gauge },
+  template: `<gauge :min="0"
+  :max="100"
+  :dp="0"
+  :unit="' €'"
+  thickness="30"
+  :svgStyle="{ maxWidth: '100%' }"
+  inactiveFill="#212121"
+  :minThreshold="80"
+  :maxThreshold="25"
+  :radius="200"
+  :pointerGap="5"
+  :pivotFill="'#3f3f3f'"
+  :pointerStroke="'#3f3f3f'"
+  :pivotStroke="'#3f3f3f'"
+  :inactiveFill="'#f3f3f3'"
+  :activeFill="'red'"
+  :pointerStrokeWidth="10"
+  pointerGap="0"
+  :value="50"  />`,
+  methods: { action: action("clicked") },
+});
